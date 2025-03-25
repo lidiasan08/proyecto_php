e8b9793d081046fcbbc766c681264606e423ca02
lidiasantos08 <lidia.santos@galileo.com>
2025-03-25 03:51:03 +0000

    first commit

---
+++ actividad3.php
@@ -0,0 +1,10 @@
+
+<?php
+$variable_nombre = "Lidia Alejandra Santos";
+echo "Hola mundo, soy Lidia Santos " . $variable_nombre;
+
+
+?>
+<br/>
+<img src="/Actividad3_seccionA/proyecto_php/imagen3.jpeg">
+
